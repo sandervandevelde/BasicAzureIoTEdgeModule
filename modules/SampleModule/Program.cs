40ff19ddb61cb508a123346fd37e152edcfce563
svelde <svelde@gmail.com>
2022-07-14 08:36:35 +0000

    initial version

---
+++ modules/SampleModule/Program.cs
@@ -0,0 +1,116 @@
+namespace SampleModule
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Runtime.Loader;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+
+    using Microsoft.Azure.Devices.Shared;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Init().Wait();
+
+            // Wait until the app unloads or is cancelled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            WhenCancelled(cts.Token).Wait();
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when app is cancelled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Initializes the ModuleClient and sets up the callback to receive
+        /// messages containing temperature information
+        /// </summary>
+        static async Task Init()
+        {
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the Edge runtime
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            Console.WriteLine("IoT Hub module client created. (for 1.1 runtime)");
+
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client started.");
+
+            // Attach callback for Twin desired properties updates
+            await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(onDesiredPropertiesUpdate, ioTHubModuleClient);
+            System.Console.WriteLine("SetDesiredPropertyUpdateCallback attached");
+
+            // Execute one-time callback method for EdgeHub Twin desired properties updates
+            var twin = await ioTHubModuleClient.GetTwinAsync();
+            await onDesiredPropertiesUpdate(twin.Properties.Desired, ioTHubModuleClient);
+            System.Console.WriteLine("SetDesiredPropertyUpdateCallback one-time executed");
+
+            // Register callback to be called when a message is received by the module
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            System.Console.WriteLine("SetInputMessageHandler 'input1' attached");
+        }
+
+        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        {
+            var moduleClient = userContext as ModuleClient;
+            if (moduleClient == null)
+            {
+                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+            }
+
+            byte[] messageBytes = message.GetBytes();
+            string messageString = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"Received Body: [{messageString}]");
+
+            if (!string.IsNullOrEmpty(messageString))
+            {
+                using (var pipeMessage = new Message(messageBytes))
+                {
+                    foreach (var prop in message.Properties)
+                    {
+                        pipeMessage.Properties.Add(prop.Key, prop.Value);
+                    }
+                    await moduleClient.SendEventAsync("output1", pipeMessage);
+                
+                    Console.WriteLine("Received message sent");
+                }
+            }
+
+            return MessageResponse.Completed;
+        }
+
+        private static async Task onDesiredPropertiesUpdate(TwinCollection desiredProperties, object userContext)
+        {
+            Console.WriteLine("One or more device twin desired properties changed:");
+            Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+        
+            var client = userContext as ModuleClient;
+        
+            var reportedProperties = new TwinCollection
+            {
+                ["DateTimeLastDesiredPropertyChangeReceived"] = DateTime.Now
+            };
+        
+            await client.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+        
+            Console.WriteLine("Sent current time as reported property to device twin");
+        }
+    }
+}
